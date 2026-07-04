a7dd7f0ab62183b05b8271b41a14a7702d097fe2
Dhiego Andrade <dhiego12052006@gmail.com>
2026-07-04 19:43:15 -0300

    UPDATE: Adding log in email

--- email_sender/email.py
+++ email_sender/email.py
@@ -8,10 +8,12 @@ def email_server(email, password):
 # Enviando email
 def send_email_template(email_smtp, title, content, email_to):
     try:
+        # Remove newlines to prevent yagmail from inserting unwanted <br> tags in the CSS style blocks
+        clean_content = content.replace("\n", "").replace("\r", "")
         email_smtp.send(
             to=email_to,
             subject=title,
-            contents=[content]
+            contents=[clean_content]
         )
         logging.info("Email enviado com sucesso")
     except Exception as e:
